26eae6348c542b94e751d8bc8dd70e186a83d11b
Mariusz Ryndzionek <mryndzionek@gmail.com>
2019-12-07 22:24:13 +0100

    Small cleanup

--- svgto3dpng.py
+++ svgto3dpng.py
@@ -5,6 +5,101 @@ import os.path
 
 import mathutils
 
+def get_objects(l):
+    return list(filter(lambda x: x.type in l, bpy.context.scene.objects))
+
+def prepare():
+    bpy.ops.object.select_all(action='DESELECT')
+
+    objs = get_objects(['MESH','CURVE'])
+
+    # delete all the existing meshes and curves
+    for obj in objs:
+        obj.select_set(True)
+        bpy.ops.object.delete()
+
+    # delete all the leftovers from previous SVG imports
+    cs = filter(lambda x: x.name.startswith('atreus'), bpy.data.collections)
+    for c in cs:
+        bpy.data.collections.remove(c)
+        
+def create(fp):
+    # import generated assembly file
+    bpy.ops.import_curve.svg(filepath = fp)
+
+    objs = get_objects(['MESH','CURVE'])
+
+    mm = 0.001
+    heights = [3.0 * mm, 6.0 * mm, 1.5 * mm, 3.0 * mm]
+    gap = 0.1 * mm
+
+    for obj in objs[len(heights):]:
+        obj.select_set(True)
+        bpy.ops.object.delete()
+
+    a = 0
+    for h, obj in zip(heights, objs):
+        bpy.context.view_layer.objects.active = None
+        bpy.context.view_layer.objects.active = obj
+        
+        obj.select_set(True)
+
+        bpy.ops.object.convert(target='MESH')
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_mode(type='FACE')
+        bpy.ops.mesh.select_all(action='SELECT')
+
+        bpy.ops.mesh.extrude_region_move(
+            TRANSFORM_OT_translate={"value":(0, 0, h)}
+        )
+
+        bpy.ops.object.mode_set(mode='OBJECT')
+        
+        bpy.ops.transform.translate(value = (0, 0, a), constraint_axis = (True, True, True))
+        a += h + gap
+        
+        obj.select_set(False)
+        
+def join():
+    objs = get_objects(['MESH'])
+    for obj in objs:
+        obj.select_set(True)
+    bpy.ops.object.join()
+    
+def move():
+    # center the keyboard in x and y
+    objs = get_objects(['MESH'])
+
+    for obj in objs:
+        obj.select_set(True)
+
+    objs = get_objects(['MESH'])
+    kbd = list(objs)[0]
+    bpy.context.scene.cursor.location = kbd.location
+    bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
+    bpy.ops.transform.translate(value=(-kbd.location.x, -kbd.location.y, 0))
+    
+def adjust_view():
+    # move camera closer
+    camera = bpy.data.objects['Camera']
+    looking_direction = camera.location - mathutils.Vector((0.0, 0.0, 0.0))
+    rot_quat = looking_direction.to_track_quat('Z', 'Y')
+    camera.rotation_euler = rot_quat.to_euler()
+    camera.location = rot_quat @ mathutils.Vector((0.0, 0.0, 0.5))
+    
+def render(rp):
+    # render scene
+    scene = bpy.context.scene
+    render = scene.render
+    render.use_file_extension = True
+    render.filepath = rp
+    bpy.ops.render.render(write_still=True)
+    
+def export(rp):
+    # create STL file
+    bpy.ops.export_mesh.stl(filepath=rp + '.stl')
+    bpy.ops.wm.save_as_mainfile(filepath='blender/' + os.path.basename(rp) + '.blend')
+
 argv = sys.argv
 if "--" not in argv:
     argv = []
@@ -18,84 +113,13 @@ if not os.path.isfile(fp):
         errno.ENOENT, os.strerror(errno.ENOENT), fp
     )
 
-bpy.ops.object.select_all(action='DESELECT')
-
-objs = filter(lambda x: x.type in ['MESH','CURVE'], bpy.context.scene.objects)
-
-# delete all the existing meshes and curves
-for obj in objs:
-    obj.select_set(True)
-    bpy.ops.object.delete()
-
-# delete all the leftovers from previous SVG imports
-cs = filter(lambda x: x.name.startswith('atreus'), bpy.data.collections)
-for c in cs:
-    bpy.data.collections.remove(c)
-
-# import generated assembly file
-bpy.ops.import_curve.svg(filepath = fp)
-
-objs = list(filter(lambda x: x.type in ['MESH','CURVE'], bpy.context.scene.objects))
-
-mm = 0.001
-heights = [3.0 * mm, 6.0 * mm, 1.5 * mm, 3.0 * mm]
-
-for obj in objs[len(heights):]:
-    obj.select_set(True)
-    bpy.ops.object.delete()
-
-a = 0
-for h, obj in zip(heights, objs):
-    bpy.context.view_layer.objects.active = None
-    bpy.context.view_layer.objects.active = obj
+rp = os.path.splitext(fp)[0]
     
-    obj.select_set(True)
+prepare()
+create(fp)
+move()
+adjust_view()
+render(rp)
+export(rp)
 
-    bpy.ops.object.convert(target='MESH')
-    bpy.ops.object.mode_set(mode='EDIT')
-    bpy.ops.mesh.select_mode(type='FACE')
-    bpy.ops.mesh.select_all(action='SELECT')
 
-    bpy.ops.mesh.extrude_region_move(
-        TRANSFORM_OT_translate={"value":(0, 0, h)}
-    )
-
-    bpy.ops.object.mode_set(mode='OBJECT')
-    
-    bpy.ops.transform.translate(value = (0, 0, a), constraint_axis = (True, True, True))
-    a += h
-    
-    obj.select_set(False)
-    
-objs = filter(lambda x: x.type in ['MESH'], bpy.context.scene.objects)
-for obj in objs:
-    obj.select_set(True)
-    
-bpy.ops.object.join()
-
-# center the keyboard in x and y
-objs = filter(lambda x: x.type in ['MESH'], bpy.context.scene.objects)
-obj = list(objs)[0]
-bpy.context.scene.cursor.location = obj.location
-bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
-bpy.ops.transform.translate(value=(-obj.location.x, -obj.location.y, 0))
-
-# move camera closer
-camera = bpy.data.objects['Camera']
-looking_direction = camera.location - mathutils.Vector((0.0, 0.0, 0.0))
-rot_quat = looking_direction.to_track_quat('Z', 'Y')
-camera.rotation_euler = rot_quat.to_euler()
-camera.location = rot_quat @ mathutils.Vector((0.0, 0.0, 0.5))
-
-# render scene
-scene = bpy.context.scene
-render_path = os.path.splitext(fp)[0]
-render = scene.render
-render.use_file_extension = True
-render.filepath = render_path
-bpy.ops.render.render(write_still=True)
-
-# create STL file
-bpy.ops.export_mesh.stl(filepath=render_path + '.stl')
-
-bpy.ops.wm.save_as_mainfile(filepath='blender/' + os.path.basename(render_path) + '.blend')
