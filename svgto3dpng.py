4a78a38021fa487c48ac0e06cca1a8c712acf736
Mariusz Ryndzionek <mryndzionek@gmail.com>
2020-09-07 19:15:19 +0200

    Use a template blender file to avoid generating objects and materials that don't change

--- svgto3dpng.py
+++ svgto3dpng.py
@@ -7,13 +7,32 @@ import mathutils
 
 mm = 0.001
 
+plates = {'bottomPlate': (1.5 * mm, 'steel'),
+          'bottomNotch': (1.0 * mm, 'wood'),
+          'spacerPlate': (8.0 * mm, 'acrylic'),
+          'switchPlate': (1.5 * mm, 'steel'),
+          'topPlate': (3 * mm, 'wood')}
+
 def get_objects(l):
 	return list(filter(lambda x: x.type in l, bpy.context.scene.objects))
+
+def load_template():
+	fn = 'template.blend'
+	objects = []
+	materials = []
+	with bpy.data.libraries.load(fn) as (data_from, _):
+		for name in data_from.objects:
+			objects.append({'name': name})
+		for name in data_from.materials:
+			materials.append({'name': name})
+
+	bpy.ops.wm.append(directory=fn+'/Object/', files=objects)
+	bpy.ops.wm.append(directory=fn+'/Material/', files=materials)
 
 def prepare():
 	bpy.ops.object.select_all(action='DESELECT')
 
-	objs = get_objects(['MESH', 'CURVE', 'LIGHT'])
+	objs = get_objects(['MESH', 'CURVE', 'LIGHT', 'CAMERA'])
 
 	# delete all the existing meshes and curves
 	for obj in objs:
@@ -30,20 +49,21 @@ def create(fp):
 	bpy.ops.import_curve.svg(filepath = fp)
 
 	objs = get_objects(['MESH','CURVE'])
-
-	heights = [1.5 * mm, 1.0 * mm, 8.0 * mm, 1.5 * mm, 3.0 * mm]
 	gap = 0.05 * mm
 
-	for obj in objs[len(heights):]:
+	for obj in objs[len(plates.keys()):]:
 		obj.select_set(True)
 		bpy.ops.object.delete()
 
 	a = 0
-	for i, (h, obj) in enumerate(zip(heights, objs)):
+	for i, (name, obj) in enumerate(zip(plates.keys(), objs)):
 		bpy.context.view_layer.objects.active = None
 		bpy.context.view_layer.objects.active = obj
 		
+		obj.name = name
 		obj.select_set(True)
+
+		(h, _) = plates[name]
 
 		bpy.ops.object.mode_set(mode='EDIT')
 		obj_data = obj.data
@@ -55,12 +75,6 @@ def create(fp):
 			a += h + gap
 		
 		obj.select_set(False)
-		
-def join():
-	objs = get_objects(['CURVE'])
-	for obj in objs:
-		obj.select_set(True)
-	bpy.ops.object.join()
 	
 def move():
 	# center the keyboard in x and y
@@ -75,102 +89,18 @@ def move():
 	bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
 	bpy.ops.transform.translate(value=(-kbd.location.x, -kbd.location.y, 0))
 
-
-def change_to_glass(obj, color):
-	TreeNodes = obj.active_material.node_tree
-	links = TreeNodes.links
-
-	# Remove nodes (clean it)
-	for node in TreeNodes.nodes:
-		TreeNodes.nodes.remove(node)
-
-	node_out = TreeNodes.nodes.new(type='ShaderNodeOutputMaterial')
-	node_out.location = 200, 0
-
-	# Glass BSDF
-	node_glass = TreeNodes.nodes.new(type='ShaderNodeBsdfGlass')
-	node_glass.distribution = 'BECKMANN'
-	node_glass.location = 0, 180
-	node_glass.distribution = 'GGX'
-	# Preserve color
-	node_glass.inputs['Color'].default_value = color
-	node_glass.inputs['Roughness'].default_value = 0.3
-	node_glass.inputs['IOR'].default_value = 1.2
-
-	links.new(node_glass.outputs[0], node_out.inputs[0])
-
-
 def adjust_materials():
 	objs = get_objects(['CURVE'])
 	for obj in objs:
-		c = obj.active_material.diffuse_color
-		obj.active_material.use_nodes = True
-		if obj.active_material == None:
-			obj.active_material = bpy.data.materials.new(name="SVGMat")
-		elif obj.name == 'Curve.002':
-			obj.active_material.blend_method = 'BLEND'
-			change_to_glass(obj, c)
-		else:
-			tn = obj.active_material.node_tree.nodes["Principled BSDF"]
-			tn.inputs['Base Color'].default_value = c
-			tn.inputs['Roughness'].default_value = 0.2
-			tn.inputs['Metallic'].default_value = 0.7
-			if obj.name == 'Curve.001':
-				tn.inputs[17].default_value = (0.06, 0.06, 0.06, 1)
-
-def adjust_view():
-	# move camera closer
-	camera = bpy.data.objects['Camera']
-	camera.location = (0.35, -0.3, 0.32)
-	looking_direction = camera.location - mathutils.Vector((0.0, 0.1, 0.0))
-	rot_quat = looking_direction.to_track_quat('Z', 'Y')
-	camera.rotation_euler = rot_quat.to_euler()
-	camera.location = rot_quat @ mathutils.Vector((0.0, 0.0, 0.5))
-
-def postprocess():
-	# create background plane
-	bpy.ops.mesh.primitive_plane_add(location=(0,0,0))
-	o = bpy.context.selected_objects[0]
-	o.active_material = bpy.data.materials.new(name="PlaneMat")
-	o.active_material.use_nodes = True
-
-	tn = o.active_material.node_tree.nodes["Principled BSDF"]
-	tn.inputs['Base Color'].default_value = (0.0420277, 0.0420277, 0.0420277, 1)
-	tn.inputs['Roughness'].default_value = 0.35
-	tn.inputs['Specular'].default_value = 0.2
-	tn.inputs['Metallic'].default_value = 0.75
-
-	light_data = bpy.data.lights.new(name="light1", type='POINT')
-	light_data.energy = 5
-	light_data.use_shadow = True
-	light_data.use_contact_shadow = True
-	light_object = bpy.data.objects.new(name="light1", object_data=light_data)
-	bpy.context.collection.objects.link(light_object)
-	bpy.context.view_layer.objects.active = light_object
-	light_object.location = (100 * mm, 90 * mm, 100 * mm)
-
-	light_data = bpy.data.lights.new(name="light2", type='POINT')
-	light_data.use_shadow = True
-	light_data.use_contact_shadow = True
-	light_data.energy = 2.5
-	light_object = bpy.data.objects.new(name="light2", object_data=light_data)
-	bpy.context.collection.objects.link(light_object)
-	bpy.context.view_layer.objects.active = light_object
-	light_object.location = (-200 * mm, 90 * mm, 100 * mm)
-
-	light_data = bpy.data.lights.new(name="light3", type='POINT')
-	light_data.use_shadow = True
-	light_data.use_contact_shadow = True
-	light_data.energy = 2.5
-	light_object = bpy.data.objects.new(name="light3", object_data=light_data)
-	bpy.context.collection.objects.link(light_object)
-	bpy.context.view_layer.objects.active = light_object
-	light_object.location = (0 * mm, 90 * mm, 100 * mm)
+		if obj.name in plates.keys():
+			(_, mat) = plates[obj.name]
+			obj.data.materials[0] = bpy.data.materials.get(mat)
 		
 def render(rp):
 	# render scene
 	scene = bpy.context.scene
 	scene.cycles.use_denoising = True
+	scene.camera = bpy.data.objects['Camera']
 	render = scene.render
 	render.use_file_extension = True
 	render.filepath = rp
@@ -199,8 +129,7 @@ rp = os.path.splitext(fp)[0]
 prepare()
 create(fp)
 move()
+load_template()
 adjust_materials()
-adjust_view()
 export(rp)
-postprocess()
 render(rp)
